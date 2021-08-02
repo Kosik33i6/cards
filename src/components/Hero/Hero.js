9a9d9126d9f78142366aad21bce11bc1ddb9155b
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-08-02 19:01:17 +0200

    Update List and Hero, add PropTypes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Hero.scss';
 
-const Hero = () => {
-    return (
-        <header className={styles.component}>
-            <h2 className={styles.title}>Things to do</h2>
-            <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="hero" />
-        </header>
-    );
+const Hero = (props) => {
+  return (
+    <header className={styles.component}>
+      <h2 className={styles.title}>{props.title}</h2>
+      <img className={styles.image} src={props.image} alt="hero" />
+    </header>
+  );
+}
+
+Hero.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.node,
 }
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
 import styles from './List.scss';
 
 class List extends Component {
-    state = {  }
-    render() {
-        return (
-            <section className={styles.component}>
-                <Hero/>
-            </section>
-        );
-    }
+  static propTypes = {
+      title: PropTypes.node,
+      children: PropTypes.node,
+      image: PropTypes.string,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
+  state = {  }
+  render() {
+    const {title, image, children} = this.props;
+    return (
+      <section className={styles.component}>
+        <Hero title={title} image={image}/>
+        {children}
+      </section>
+    );
+  }
 }
 
 export default List;
