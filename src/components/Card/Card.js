7f04a321d93b3b7615f972ec01ca1e0fdb385629
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-08-03 12:12:03 +0200

    Add cards for columns, update columns

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Card.scss';
+
+const Card = (props) => {
+  const {title} = props;
+  return (
+    <div className={styles.component}>{title}</div>
+  );
+}
+
+Card.propTypes = {
+  title: PropTypes.node
+}
+
+export default Card;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import Icon from '../Icon/Icon';
 import styles from './Column.scss';
 
 class Column extends Component {
   static propTypes = {
     title: PropTypes.node,
+    cards: PropTypes.array,
+    icon: PropTypes.string,
   }
-  state = {  }
+  state = {
+    cards: this.props.cards || [],
+  }
   render() {
-    const {title} = this.props;
+    console.log(this.props);
+    const {title, icon} = this.props;
+    const {cards} = this.state;
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{title}</h3>
+        <h3 className={styles.title}>{title} <span className={styles.icon}><Icon name={icon}/></span></h3>
+        {cards.map(cardData => (
+          <Card key={cardData.key} {...cardData}/>
+        ))}
       </section>
     );
   }
